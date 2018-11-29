e19d84a5ea59942e9c44c1be23ef8d96f676b5fd
Lorenz <Lorenz@DESKTOP-E8C13G3>
2018-11-29 20:13:16 +0100

    Bewertungsseite mit Funktionen

--- PEPManagement/src/BewertungServlet.java
+++ PEPManagement/src/BewertungServlet.java
@@ -12,12 +12,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/BerwertungServlet")
-public class BerwertungServlet extends HttpServlet {
+@WebServlet("/BewertungServlet")
+public class BewertungServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	Database db;
 	
-    public BerwertungServlet() {
+    public BewertungServlet() {
         super();
         
         db = new Database();
@@ -44,12 +44,34 @@ public class BerwertungServlet extends HttpServlet {
             request.getRequestDispatcher("bewertung.jsp").forward(request, response);
             
         } catch (SQLException e) {
-			System.out.println("SQLError in BewertungsServlet.java: " + e.getMessage());		
+			System.out.println("SQLError in BewertungServlet.java: " + e.getMessage());		
         }
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
+		String hauptkriterium = request.getParameter("hauptkriterium");
+		String teilkriterium = request.getParameter("teilkriterium");
+		String maxpunkte =request.getParameter("maxpunkte");
+		String page = "";		
+		System.out.println(hauptkriterium + " " + teilkriterium + " " + maxpunkte );
+		
+//		TODO: Fehlerbehandlung in der JSP 
+		if(hauptkriterium == null || teilkriterium == null || maxpunkte == null) {
+			System.out.println("Error null");
+			page = "bewertung.jsp?error=1";			
+		} else {			
+			try {
+				db.addKriterium(hauptkriterium, teilkriterium, Integer.parseInt(maxpunkte));
+				page = "BewertungServlet";
+				System.out.println("geschafft");
+				
+			} catch (SQLException e) {
+				System.out.println("SQLError in BewertungskriteriumServlet.java: " + e.getMessage());
+				page = "bewertung.jsp?error=1";
+			}
+		}
+		doGet(request, response);
 	}
 
 }
